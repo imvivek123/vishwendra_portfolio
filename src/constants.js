d30500f2431fbdeb3addb09cba3ab123a3627520
imvivek123 <147731166+imvivek123@users.noreply.github.com>
2025-07-11 12:52:45 +0530

    Initial commit with portfolio files

---
+++ src/constants.js
@@ -0,0 +1,219 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+   
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+    
+   
+      { name: 'kotlin', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+    
+      { name: 'Dart flutter', logo: materialuiLogo },
+     
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+     
+    
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+    
+      { name: 'JavaScript', logo: javascriptLogo },
+     
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Figma', logo: figmaLogo },
+       { name: 'powerbi', logo: netlifyLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    
+    
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Chemical Engineering Intern",
+  company: "Spes Bona Engineering",
+  date: "june 2025 - July 2025",
+  desc: "Worked as a Chemical Engineering Intern, gaining practical exposure to industrial processes and sustainable technologies. Involved in understanding process design, mass and energy balances, and safety protocols. Focused on real-world applications of green hydrogen and green ammonia production, along with exposure to equipment design and simulation tools used in process engineering.",
+  skills: [
+    "Process Design",
+    "Mass & Energy Balances",
+    "Green Hydrogen",
+    "Fired heater, API 560,530",
+    "Safety Protocols",
+    "Chemical Process Simulation"
+      ],
+    },
+  ];
+  
+  export const education = [
+   
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "Indian Institute of Technology Jodhpur",
+      date: "August 2023 - may 2027",
+     
+      desc: "I am currently pursuing my B.Tech in Chemical Engineering at IIT Jodhpur, where I am building a strong foundation in both core engineering principles and modern technological skills. Alongside my coursework in subjects like thermodynamics, transport phenomena, and reaction engineering, I have developed a keen interest in the tech domain. I’ve actively explored areas such as data structures and algorithms, app development, and data analysis, allowing me to work on interdisciplinary projects that merge engineering with software solutions. My experience at IIT Jodhpur has helped me grow as a problem-solver capable of applying both analytical thinking and technical tools to real-world challenges..",
+      degree: "Bachelor of Technology(Btech)",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Divine International Academy",
+      date: "Apr 2021- March 2022",
+      grade: "94%",
+      desc: "I completed my class 12 education from Divine International Academy, Sirsaganj, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "CBSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: vpsLogo,
+      school: "GR Convent School ",
+      date: "Apr 2019 - March 2020",
+      grade: "88%",
+      desc: "I completed my class 10 education from Gr Convent School, Ghaziabad, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    
+   
+    
+    {
+      id: 3,
+      title: "PORTFOLIO WEBSITE",
+      description:
+        "A responsive and interactive portfolio website developed using React.js, Tailwind CSS, JavaScript, HTML, and CSS. It features smooth animations, section-based navigation, and visually appealing layouts to effectively showcase projects, skills, and contact information. Designed with a mobile-first approach and enhanced with animated effects, it delivers a seamless user experience across devices.",
+      image: npmLogo,
+      tags: ["React JS", "Tailwind CSS", "JavaScript", "HTML", "CSS", "Animations"],
+      github: "https://github.com/codingmastr/cmtk-email-validator",
+      
+    },
+    {
+      id: 4,
+      title: "blinkit-powerbi-dashboard",
+      description:
+        "Developed an interactive Power BI dashboard to analyze and visualize Blinkit's operational and sales data. The dashboard provides insights into order volumes, delivery efficiency, customer behavior, and regional performance, enabling data-driven decision-making for business optimization,.",
+      image: taskremLogo,
+      tags: ["Power BI", "Data Analysis", "Data Visualization", "Business Intelligence", "Excel", "DAX","MYSQL" ],
+      github: "https://github.com/imvivek123/blinkit-powerbi-dashboard",
+   
+    },
+    {
+      id: 5,
+      title: "Student performance Analysis",
+      description:
+        "A machine learning-based project aimed at analyzing and predicting student academic performance based on various socio-economic and educational factors. Implemented multiple ML algorithms using Python in Jupyter Notebook and built a web interface for input and result visualization",
+      image: webverLogo,
+      tags: ["python", "machine Learning", "Jupitor notebook", "html","css"],
+      github: "https://github.com/imvivek123/IML-Project",
+     
+    },
+    
+    {
+      id: 7,
+      title: "College Sync App",
+      description:
+        "A student-centric mobile application designed to help first-year college students access academic resources, previous year papers, notes, and essential campus information. The app also includes a structured overview of college clubs, societies, and key student contacts. Built using Kotlin for Android with Firebase integration for real-time data storage..",
+      image: imagesearchLogo,
+      tags: ["Kotlin", "Firebase", "Android Development", "UI/UX", "Student Resources"],
+  github: "https://github.com/yourusername/College-Sync-App",
+     
+    },
+    {
+      id: 8,
+      title: "Image Segamantation using graph cut",
+      description:
+        "Developed an image segmentation tool that utilizes the Graph Cut algorithm for efficient foreground-background separation. Implemented in C++ with OpenCV for backend processing and React.js for the frontend interface. Users can upload images, perform precise segmentation, and visualize the results with an intuitive UI",
+      image: removebgLogo,
+      tags: ["dsa" , "C++", "open CV", "Graph cut Algo", ],
+      github: "https://github.com/imvivek123/Graph-Based-Foreground-Extraction",
+      
+    },
+  ];  
